e7467b0e20c8469db7caaa438ef4f6de1535ba17
ombhojane <ombhojane05@gmail.com>
2024-10-25 19:05:40 +0530

    Smile Init

---
+++ app/page.js
@@ -0,0 +1,30 @@
+import { Suspense } from 'react'
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Problem from "@/components/Problem";
+import FeaturesAccordion from "@/components/FeaturesAccordion";
+import Pricing from "@/components/Pricing";
+import FAQ from "@/components/FAQ";
+import CTA from "@/components/CTA";
+import Footer from "@/components/Footer";
+import ButtonLead from "@/components/ButtonLead";
+
+export default function Home() {
+  return (
+    <>
+      <Suspense>
+        <Header />
+      </Suspense>
+      <main>
+        <Hero />
+        <Problem />
+        <FeaturesAccordion />
+        <Pricing />
+        <FAQ />
+        <CTA />
+        <ButtonLead />        
+      </main>
+      <Footer />
+    </>
+  );
+}
